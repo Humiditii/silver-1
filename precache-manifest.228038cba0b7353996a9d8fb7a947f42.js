cfe408447a73afb0a7cf6d96941f5889727417a7
Babatunde Abdulhameed Olakunle <humiditii45@gmail.com>
2020-07-04 17:17:59 +0000

    Updates

---
+++ precache-manifest.228038cba0b7353996a9d8fb7a947f42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66f481a94be7907596ea012821c6742b",
+    "url": "/silver-1/index.html"
+  },
+  {
+    "revision": "f79f81765b893724e3d1",
+    "url": "/silver-1/static/css/2.5a510433.chunk.css"
+  },
+  {
+    "revision": "f298531b3bc91f948bcc",
+    "url": "/silver-1/static/css/main.f3829fc3.chunk.css"
+  },
+  {
+    "revision": "f79f81765b893724e3d1",
+    "url": "/silver-1/static/js/2.faa779ff.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/silver-1/static/js/2.faa779ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f298531b3bc91f948bcc",
+    "url": "/silver-1/static/js/main.644fb102.chunk.js"
+  },
+  {
+    "revision": "5719f702db9dacf02852",
+    "url": "/silver-1/static/js/runtime-main.d83c1f7f.js"
+  }
+]);
