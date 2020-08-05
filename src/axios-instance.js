870a9d6c07fa818edc7d2b3567a124d4a4e284e8
Babatunde Abdulhameed Olakunle <humiditii45@gmail.com>
2020-08-05 23:14:17 +0000

    changing axios baseUrl to remote

--- src/axios-instance.js
+++ src/axios-instance.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3333/api/v1'
-    //baseURL: 'https://silver1-api.herokuapp.com/api/v1'
+    //baseURL: 'http://localhost:3333/api/v1'
+    baseURL: 'https://silver1-api.herokuapp.com/api/v1'
 });
 
 export default instance;
